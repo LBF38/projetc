1cdc4948332fd89f2d8fda6631b6c33dd50fa46a
LBF38 <mathis.urien@free.fr>
2022-10-07 10:46:09 +0200

    Debugged the pop function Added a copy_cell function

--- src/main.c
+++ src/main.c
@@ -2,16 +2,28 @@
 
 #include "list.h"
 
-int main(int argc, char *argv[]) {
-    struct list* l;
+int main(int argc, char *argv[])
+{
+    struct list *l;
     l = new_list();
     print_list(l);
 
-    struct cell* c;
+    struct cell *c;
     c = make_cell("Annie", "ERNAUX", "76170");
-    
+
     push(l, c);
 
+    print("List status\n");
+    print_list(l);
+
+    struct cell *out;
+    out=malloc(sizeof(struct cell));
+    pop(l,out);
+
+    print("out cell content : \n");
+    print_cell(out);
+
+    print("List status\n");
     print_list(l);
 
     free_list(l);
