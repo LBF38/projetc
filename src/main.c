0d6824924863626c9536efe0771b666076ed814b
LBF38 <mathis.urien@free.fr>
2022-10-17 17:07:08 +0200

    Ajout de fonctionnalités CLI pour maitriser les affichages de tests des méthodes

--- src/main.c
+++ src/main.c
@@ -5,52 +5,93 @@
 int main(int argc, char *argv[])
 {
     int display_mode = 0;
+    int method1 = 0;
+    int method2 = 0;
+    int default_mode = 1;
     if (argc < 2)
     {
-        printf("Please include a path to the file you want to open.\n");
+        printf("Please include a path to the file you want to open.\n\n"
+               "Usage : ./bin/womc [path/to/file.txt] [-options]\n\n"
+               "Available options :\n\n"
+               "-d,--display          Display the list resulting of the method used\n"
+               "-m1,--method1         Launch only the method 1. Compatible with the display option.\n"
+               "-m2,--method2         Launch only the method 2. Compatible with the display option.\n"
+               "--default             Launch both methods in default mode. Disable the display option.\n"
+               );
+
         exit(-1);
     }
     if (argc > 2 && argv[2] != NULL)
     {
-        if (strcmp(argv[2], "--display") == 0)
+        for (int i = 2; i < argc; i++)
         {
-            display_mode = 1;
+            printf("%s\n", argv[i]);
+            if (strcmp(argv[i], "--display") == 0 || strcmp(argv[i], "-d") == 0)
+            {
+                display_mode = 1;
+            }
+            if (strcmp(argv[i], "--method1") == 0 || strcmp(argv[i], "-m1") == 0)
+            {
+                method1 = 1;
+                method2 = 0;
+                default_mode=0;
+            }
+            if (strcmp(argv[i], "--method2") == 0 || strcmp(argv[i], "-m2") == 0)
+            {
+                method1 = 0;
+                method2 = 1;
+                default_mode=0;
+            }
+            if (strcmp(argv[i], "--default") == 0)
+            {
+                default_mode = 1;
+                method1 = 0;
+                method2 = 0;
+                display_mode = 0;
+            }
         }
     }
     char *file = argv[1];
 
     if (file)
     {
-        printf("Méthode 1 :\n\n");
-        printf("Start loading file using method 1\n");
-        clock_t function_time;
-        function_time = clock();
-        struct list *lst;
-        lst = load_file(file);
-        if (display_mode)
+        if (method1 || default_mode)
         {
-            print_list(lst);
+            printf("Méthode 1 :\n\n");
+            printf("Start loading file using method 1\n");
+            clock_t function_time;
+            function_time = clock();
+            struct list *lst;
+            lst = load_file(file);
+            if (display_mode)
+            {
+                print_list(lst);
+            }
+            free_list(lst);
+            function_time = clock() - function_time;
+            printf("Ended loading file using method 1\n");
+            double time_taken = ((double)function_time) / CLOCKS_PER_SEC;
+            printf("Time used by method 1 : %f seconds\n", time_taken);
         }
-        free_list(lst);
-        function_time = clock() - function_time;
-        printf("Ended loading file using method 1\n");
-        double time_taken = ((double)function_time) / CLOCKS_PER_SEC;
-        printf("Time used by method 1 : %f seconds\n", time_taken);
 
-        printf("\nMéthode 2 :\n\n");
-        printf("Start loading file using method 2\n");
-        function_time = clock();
-        struct llist *llst;
-        llst = load_file_optimized(file);
-        if (display_mode)
+        if (method2 || default_mode)
         {
-            print_llist(llst);
+            printf("\nMéthode 2 :\n\n");
+            printf("Start loading file using method 2\n");
+            clock_t function_time2;
+            function_time2 = clock();
+            struct llist *llst;
+            llst = load_file_optimized(file);
+            if (display_mode)
+            {
+                print_llist(llst);
+            }
+            free_llist(llst);
+            function_time2 = clock() - function_time2;
+            printf("Ended loading file using method 2\n");
+            double time_taken2 = ((double)function_time2) / CLOCKS_PER_SEC;
+            printf("Time used by method 2 : %f seconds\n", time_taken2);
         }
-        free_llist(llst);
-        function_time = clock() - function_time;
-        printf("Ended loading file using method 2\n");
-        time_taken = ((double)function_time) / CLOCKS_PER_SEC;
-        printf("Time used by method 2 : %f seconds\n", time_taken);
     }
 
     return 0;
